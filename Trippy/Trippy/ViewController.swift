262626923cc55f409cefeffe8f4945915bc4d3ec
Denis Cherniy <d.chernyi@storytelling-software.com>
2021-05-22 10:42:27 +0300

    Created project.

---
+++ Trippy/Trippy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trippy
+//
+//  Created by Denis Cherniy on 22.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
