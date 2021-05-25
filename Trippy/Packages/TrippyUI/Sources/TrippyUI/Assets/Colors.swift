e12ab443286ac691393347cc098ae8159da87d11
mentevali <denis.chernyy888@gmail.com>
2021-05-25 21:00:26 +0300

    Added new colors.

--- Trippy/Packages/TrippyUI/Sources/TrippyUI/Assets/Colors.swift
+++ Trippy/Packages/TrippyUI/Sources/TrippyUI/Assets/Colors.swift
@@ -8,5 +8,16 @@
 public extension Asset {
     enum Color {
         public static var primaryBackground = ColorAsset(name: "VioletBlue")
+        public static let accent = ColorAsset(name: "SunYellow")
+        
+        public enum Text {
+            public static let primary = ColorAsset(name: "White")
+            public static let accent = ColorAsset(name: "SunYellow")
+        }
+        
+        public enum Button {
+            public static var background = ColorAsset(name: "SunYellow")
+            public static var text = ColorAsset(name: "Indigo")
+        }
     }
 }
